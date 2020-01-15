b1bbeb5ea5714e154fbf52d2ef762177521f4919
thembones79 <michal.szumnarski@gmail.com>
2020-01-15 21:42:08 +0100

    done

--- src/actions.js
+++ src/actions.js
@@ -22,17 +22,26 @@ export function addAnEventToTheBeginningOfTheArray() {
   };
 }
 export function addAnEventToTheEndOfTheArray() {
-  return { type: ADD_AN_EVENT_TO_THE_END_OF_THE_ARRAY };
+  return {
+    type: ADD_AN_EVENT_TO_THE_END_OF_THE_ARRAY,
+    payload: { id: increment(), type: "REFUND", value: -6.99 }
+  };
 }
 export function insertAnEventAfterTheFirstItem() {
-  return { type: INSERT_AN_EVENT_AFTER_THE_FIRST_ITEM };
+  return {
+    type: INSERT_AN_EVENT_AFTER_THE_FIRST_ITEM,
+    payload: { id: increment(), type: "SALE", value: 0.99 }
+  };
 }
 export function removeTheSecondEventFromTheList() {
   return { type: REMOVE_THE_SECOND_EVENT_FROM_THE_LIST };
 }
 export function removeTheEventWithId1() {
-  return { type: REMOVE_THE_EVENT_WITH_ID_1 };
+  return { type: REMOVE_THE_EVENT_WITH_ID_1, payload: { id: 1 } };
 }
 export function produceAnArrayThatOnlyContainsSales() {
-  return { type: PRODUCE_AN_ARRAY_THAT_ONLY_CONTAINS_SALES };
+  return {
+    type: PRODUCE_AN_ARRAY_THAT_ONLY_CONTAINS_SALES,
+    payload: { type: "SALE" }
+  };
 }
--- src/index.js
+++ src/index.js
@@ -16,6 +16,22 @@ function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case actions.ADD_AN_EVENT_TO_THE_BEGINNING_OF_THE_ARRAY:
       return [action.payload, ...state];
+    case actions.ADD_AN_EVENT_TO_THE_END_OF_THE_ARRAY:
+      return [...state, action.payload];
+    case actions.INSERT_AN_EVENT_AFTER_THE_FIRST_ITEM:
+      return [...state.slice(0, 1), action.payload, ...state.slice(2)];
+    case actions.REMOVE_THE_SECOND_EVENT_FROM_THE_LIST:
+      return state.filter((item, index) => {
+        return index !== 1;
+      });
+    case actions.REMOVE_THE_EVENT_WITH_ID_1:
+      return state.filter(item => {
+        return item.id !== action.payload.id;
+      });
+    case actions.PRODUCE_AN_ARRAY_THAT_ONLY_CONTAINS_SALES:
+      return state.filter(item => {
+        return item.type === action.payload.type;
+      });
 
     default:
       return state;
